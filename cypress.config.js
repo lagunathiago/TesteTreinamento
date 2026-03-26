3b2325c8d57240c0ac853300c54da02dd56e8874
lagunathiago <thiagolaguna8@gmail.com>
2026-03-26 10:05:29 -0300

    'teste2

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-        testIsolation: false,            // ⬅️ mantém o estado/URL ao iniciar um novo `it`
-        viewportWidth: 1920,             // (opcional) mesma largura que você usa
-    viewportHeight: 1080,            // (opcional)
+    testIsolation: false,
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+
+    // 👇 ADICIONADO (igual do vídeo)
+    video: true,
+    screenshotOnRunFailure: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
