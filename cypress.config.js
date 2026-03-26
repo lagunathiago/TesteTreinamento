104c73f9490fad16c6aa34cd20577944f0aabcb8
lagunathiago <thiagolaguna8@gmail.com>
2026-03-26 14:41:37 -0300

    atomizaçã

--- cypress.config.js
+++ cypress.config.js
@@ -6,12 +6,13 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     viewportHeight: 1080,
 
-    // 👇 ADICIONADO (igual do vídeo)
     video: true,
+    videoCompression: false, // 🔥 ESSA LINHA FAZ DIFERENÇA
     screenshotOnRunFailure: true,
 
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    defaultCommandTimeout: 10000, // evita espera exagerada
+    pageLoadTimeout: 30000,
+
+    setupNodeEvents(on, config) {},
   },
 });
