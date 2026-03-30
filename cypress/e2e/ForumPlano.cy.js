68753e30bc332ba37f3821137e7171e8a6e63ebf
lagunathiago <thiagolaguna8@gmail.com>
2026-03-30 17:11:53 -0300

    'teste5

--- cypress/e2e/ForumPlano.cy.js
+++ cypress/e2e/ForumPlano.cy.js
@@ -31,12 +31,12 @@ describe("Teste - Login", () => {
     cy.get('form.ng-pristine > [type="text"]', { timeout: 60000 })
       .scrollIntoView()
       .should("be.visible")
-      .type("thiagosuporte@uorak.com");
+      .type("qualidade2@lectortec.com.br");
 
     cy.get("ng-transclude > .border", { timeout: 60000 })
       .scrollIntoView()
       .should("be.visible")
-      .type("123");
+      .type("2006lrnrgr");
 
     cy.get("#btn-entrar", { timeout: 60000 })
       .scrollIntoView()
@@ -62,8 +62,8 @@ describe("Teste - Login", () => {
       .click({force: true})
 
      });
-
-      it('Cria o treinamento Plano de Tutoria Automação', () => {
+/*
+      it('Cria o treinamento Forum Automação', () => {
         
         //Clicar em criar treinamentos
         cy.get('.title-bar .btn-icon', { timeout: 600000 }) // até 10 minutos
@@ -73,7 +73,7 @@ describe("Teste - Login", () => {
     .click({ force: true });
 
         cy.get("#courseName").click(); // Clica pra digitar
-        cy.get("#courseName").type("Plano de Tutoria Automação") //  Nome no Treinamento
+        cy.get("#courseName").type("Forum Automação") //  Nome no Treinamento
 
     })
 
@@ -88,7 +88,6 @@ describe("Teste - Login", () => {
             cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
             .should('be.visible')
             .click();
-
 
       cy.get(".editing-resource > :nth-child(2) > .w-100").click();
       cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
@@ -124,18 +123,32 @@ describe("Teste - Login", () => {
       cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
      cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
 
-     cy.pause()
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
       
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       
        cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
-      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default').type("thiago laguna")
-      cy.get('.ui-select-dropdown')
-  .should('be.visible')
+     
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
 
-cy.contains('.ui-select-choices-row', 'Thiago Laguna')
-  .click()
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
 
       cy.contains('button', 'Adicionar')
   .should('be.visible')
@@ -153,8 +166,7 @@ cy.contains('.ui-select-choices-row', 'Thiago Laguna')
       cy.wait(5000)
 
     });
-    
-    /*
+    */
       it('Cria o treinamento Tutoria Aprovado Automação', () => {
         
         //Clicar em criar treinamentos
@@ -180,7 +192,6 @@ cy.contains('.ui-select-choices-row', 'Thiago Laguna')
             cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
             .should('be.visible')
             .click();
-
 
       cy.get(".editing-resource > :nth-child(2) > .w-100").click();
       cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
@@ -215,17 +226,34 @@ cy.contains('.ui-select-choices-row', 'Thiago Laguna')
       cy.get('[ng-click="editClass()"]').click() //Nova turma
       cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
      cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
+
+     
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
       
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       
        cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
-      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default').type("thiago laguna")
-      cy.get('.ui-select-dropdown')
-  .should('be.visible')
+    
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
 
-cy.contains('.ui-select-choices-row', 'Thiago Laguna')
-  .click()
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
 
       cy.contains('button', 'Adicionar')
   .should('be.visible')
@@ -304,17 +332,138 @@ cy.contains('.ui-select-choices-row', 'Thiago Laguna')
       cy.get('[ng-click="editClass()"]').click() //Nova turma
       cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
      cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
+
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
       
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       cy.get('.navigation-controls > .ml-20').click()//botao prximo
       
        cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
-      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default').type("thiago laguna")
-      cy.get('.ui-select-dropdown')
-  .should('be.visible')
+   
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
 
-cy.contains('.ui-select-choices-row', 'Thiago Laguna')
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
+
+      cy.contains('button', 'Adicionar')
+  .should('be.visible')
   .click()
+
+      cy.wait(1000)
+     
+      // Clica no botão "Salvar Turma"
+      
+      cy.get('.add-content > .end > .btn-swipe-accent').click()
+
+      
+      cy.get('.content-box-footer > .flex > .btn-swipe-accent').click()
+
+            cy.wait(5000)
+      
+    });
+
+      it('Cria o treinamento Tutoria Prorrogação', () => {
+        
+        //Clicar em criar treinamentos
+        cy.get('.title-bar .btn-icon', { timeout: 600000 }) // até 10 minutos
+    .should('exist')
+    .should('be.visible')
+    .and('not.be.disabled')
+    .click({ force: true });
+
+        cy.get("#courseName").click(); // Clica pra digitar
+        cy.get("#courseName").type("Tutoria Prorrogação Automação") //  Nome no Treinamento
+    
+    })
+
+     it("Conteúdo - Documento JPEG", () => {
+
+      //Clica em conteudos
+            cy.get('[ui-sref="accessLink.content.courses.edit.id.contents"]', {timeout:10000})
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+
+            cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+
+      cy.get(".editing-resource > :nth-child(2) > .w-100").click();
+      cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
+
+      cy.contains(".ui-select-container", "Escolha um documento")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 10000 })
+        .should("have.length", 1)
+        .type("CAPA 19.jpg", { delay: 10 });
+
+      cy.contains(
+        ".ui-select-choices-row",
+        "CAPA 19.jpg",
+        { timeout: 10000 },
+      )
+        .should("be.visible")
+        .click({ force: true });
+
+        //Peso 1
+        cy.get(".weight").type("1");                                                   // Selecionar peso
+        cy.get(".open > .ui-select-choices > :nth-child(2)").click();                  // Selecionar peso 1
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+
+    });
+  
+    it('Turma Gratuita sem aprovação', () => {
+
+      cy.get('[ui-sref="accessLink.content.courses.edit.id.classes"]').click()
+      cy.get('[ng-click="editClass()"]').click() //Nova turma
+      cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
+     cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
+
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
+      
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      
+       cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
+   
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
+
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
 
       cy.contains('button', 'Adicionar')
   .should('be.visible')
@@ -331,13 +480,293 @@ cy.contains('.ui-select-choices-row', 'Thiago Laguna')
 
             cy.wait(5000)
 
+    });
+
+       it('Cria o treinamento Tutoria Progresso', () => {
+        
+        //Clicar em criar treinamentos
+        cy.get('.title-bar .btn-icon', { timeout: 600000 }) // até 10 minutos
+    .should('exist')
+    .should('be.visible')
+    .and('not.be.disabled')
+    .click({ force: true });
+
+        cy.get("#courseName").click(); // Clica pra digitar
+        cy.get("#courseName").type("Tutoria Progresso Automação") //  Nome no Treinamento
+    
+    })
+
+     it("Conteúdo - Documento JPEG", () => {
+
+      //Clica em conteudos
+            cy.get('[ui-sref="accessLink.content.courses.edit.id.contents"]', {timeout:10000})
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+
+            cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+      cy.get(".editing-resource > :nth-child(2) > .w-100").click();
+      cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
+
+      cy.contains(".ui-select-container", "Escolha um documento")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 10000 })
+        .should("have.length", 1)
+        .type("CAPA 19.jpg", { delay: 10 });
+
+      cy.contains(
+        ".ui-select-choices-row",
+        "CAPA 19.jpg",
+        { timeout: 10000 },
+      )
+        .should("be.visible")
+        .click({ force: true });
+
+        //Peso 1
+        cy.get(".weight").type("1");                                                   // Selecionar peso
+        cy.get(".open > .ui-select-choices > :nth-child(2)").click();                  // Selecionar peso 1
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+
+    });
+
+    
+    it("Conteúdo - Documento PPTX", () => {
+
+         cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+      cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
+
+      cy.contains(".ui-select-container", "Escolha um documento")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 60000 })
+        .should("have.length", 1)
+        .type("Certificado 14.02.2024", { delay: 10 });
+
+      cy.contains(".ui-select-choices-row", "Certificado 14.02.2024", {
+        timeout: 60000,
+      })
+        .should("be.visible")
+        .click({ force: true });
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+    });
+
+    it("Conteúdo - Documento DOC", () => {
+
+            cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+      cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
+
+      cy.contains(".ui-select-container", "Escolha um documento")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 60000 })
+        .should("have.length", 1)
+        .type("6518 - Opcao de evento hibrido", { delay: 10 });
+
+      cy.contains(".ui-select-choices-row", "6518 - Opcao de evento hibrido", {
+        timeout: 60000,
+      })
+        .should("be.visible")
+        .click({ force: true });
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+    });
+
+    it("Conteúdo - Avaliação (uma por página)", () => {
+
+            cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+      cy.get(".open > .ui-select-choices > :nth-child(3)").click(); // Avaliação
+
+      cy.contains(".ui-select-container", "Escolha uma avaliação")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 60000 })
+        .should("have.length", 1)
+        .type("uma por página Thiago", { delay: 10 });
+
+      cy.contains(".ui-select-choices-row", "uma por página Thiago", {
+        timeout: 60000,
+      })
+        .should("be.visible")
+        .click({ force: true });
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+
+    });
+  
+    it('Turma Gratuita sem aprovação', () => {
+
+      cy.get('[ui-sref="accessLink.content.courses.edit.id.classes"]').click()
+      cy.get('[ng-click="editClass()"]').click() //Nova turma
+      cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
+     cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
+
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
+      
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      
+       cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
+   
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
+
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
+
+      cy.contains('button', 'Adicionar')
+  .should('be.visible')
+  .click()
+
+      cy.wait(1000)
+     
+      // Clica no botão "Salvar Turma"
+      
+      cy.get('.add-content > .end > .btn-swipe-accent').click()
+
+      
+      cy.get('.content-box-footer > .flex > .btn-swipe-accent').click()
+
+            cy.wait(5000)
+
+    });
+
+       it('Cria o treinamento Tutoria Tempo de Inatividade', () => {
+        
+        //Clicar em criar treinamentos
+        cy.get('.title-bar .btn-icon', { timeout: 600000 }) // até 10 minutos
+    .should('exist')
+    .should('be.visible')
+    .and('not.be.disabled')
+    .click({ force: true });
+
+        cy.get("#courseName").click(); // Clica pra digitar
+        cy.get("#courseName").type("Tutoria Inatividade Automação") //  Nome no Treinamento
+    
+    })
+
+     it("Conteúdo - Documento JPEG", () => {
+
+      //Clica em conteudos
+            cy.get('[ui-sref="accessLink.content.courses.edit.id.contents"]', {timeout:10000})
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+
+            cy.get('ui-view.ng-scope > .flex > .btn-swipe-accent', {timeout:10000})
+            .should('be.visible')
+            .click();
+
+      cy.get(".editing-resource > :nth-child(2) > .w-100").click();
+      cy.get(".open > .ui-select-choices > :nth-child(2)").click(); // Documentos
+
+      cy.contains(".ui-select-container", "Escolha um documento")
+        .should("be.visible")
+        .click();
+
+      cy.get("input.ui-select-search:visible", { timeout: 10000 })
+        .should("have.length", 1)
+        .type("CAPA 19.jpg", { delay: 10 });
+
+      cy.contains(
+        ".ui-select-choices-row",
+        "CAPA 19.jpg",
+        { timeout: 10000 },
+      )
+        .should("be.visible")
+        .click({ force: true });
+
+        //Peso 1
+        cy.get(".weight").type("1");                                                   // Selecionar peso
+        cy.get(".open > .ui-select-choices > :nth-child(2)").click();                  // Selecionar peso 1
+
+      cy.get(".editing-resource > .end > .btn-swipe-accent").click();
+
+    });
+  
+    it('Turma Gratuita sem aprovação', () => {
+
+      cy.get('[ui-sref="accessLink.content.courses.edit.id.classes"]').click()
+      cy.get('[ng-click="editClass()"]').click() //Nova turma
+      cy.get("#className").type("Turma Gratuita sem aprovação"); //nome da turma
+     cy.get('.column > :nth-child(1) > .icon-checkbox').click(); // desativa aprovação
+
+     //Clica nos Tutores
+     cy.get(':nth-child(7) > .multiselect > [name="UserOwnerSelect"] > :nth-child(1) > .ui-select-search')
+     .scrollIntoView()
+     .click()
+
+     cy.wait(2000)
+
+     //Clica em Thiago Laguna
+     cy.contains('.ui-select-choices-row','Thiago Laguna', {timeout:10000})
+     .click()
+
+    cy.wait(2000)
+      
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      cy.get('.navigation-controls > .ml-20').click()//botao prximo
+      
+       cy.get('tr.ng-scope > :nth-child(4) > .middle > .btn').click()
+   
+      cy.get('.step2 > .permission-select > [ng-show="showUser"] > .column > .multiselect > .border > .ui-select-match > .btn-default')
+        .type("Aluno")
+
+        cy.wait(1000)
+
+         cy.get('.ui-select-choices-row:visible', { timeout: 20000 })
+  .contains(/^Aluno$/)
+  .click({ force: true })
+
+      cy.contains('button', 'Adicionar')
+  .should('be.visible')
+  .click()
+
+      cy.wait(1000)
+     
+      // Clica no botão "Salvar Turma"
+      
+      cy.get('.add-content > .end > .btn-swipe-accent').click()
+
+      
+      cy.get('.content-box-footer > .flex > .btn-swipe-accent').click()
+
+            cy.wait(5000)
       
     });
-*/
-
-
-
-
 
   });
+
 });
